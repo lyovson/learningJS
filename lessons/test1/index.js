618137e8d5dea8bdbf0122ffabeb56e337124b6d
lyovson <lyovson@icloud.com>
2022-05-02 21:52:15 +0400

    added test1

---
+++ lessons/test1/index.js
@@ -0,0 +1,61 @@
+// I wrote file and variable names in curly braces {} for clarity, skip them while naming
+
+// clone this repo to your pc
+
+// create a folder named { utils } in this folder with an { index.js } files in it
+
+// declare 4 variables using 4 ways allowed in JS containing values of 4 different data types in /utils/index.js
+
+// declare 3 functions doing addition, subtraction and multiplication of 2 numbers using 3 ways allowed in JS in /utils/index.js
+
+// declare an object { utils } with a method to divide 2 numbers in /utils/index.js
+
+// add the 3 functions created outside of the { utils } object to the object without re-creating them
+
+// add a method to the object utils called { range }that takes 2 numbers and creates an array containing all the numbers between those 2 in /utils/index.js
+
+// add a method { curry } to the object { utils } that takes a function taking multiple argumets and returns a function that can take the same amount of arguments one at a time in /utils/index.js
+
+// export the object utils using both named and default export methods
+
+// create a file in /test1 folder named { index.js }
+
+// import the { utils } object using the default import and the { range } and { curry } methods using named import on the same line in /index.js
+
+// partially apply number 2 to the basic math methods of the object utils using the { curry } method of the same object and save them in 4 new variables in /index.js
+
+// create an array of numbers 1 to  10 using the { range } method of the object { utils } in /index.js
+
+// apply the 4 partially applied functions to the newly created array and save the results to 4 new variables in /index.js
+
+// log to the console the results of those 4 variables in index.js
+
+// create a new file in /test1 folder named { students.js }
+
+// add 2 new methods { capitalize } and { uppercase } to the object { utils } that take a string and respectively capitalize and uppercase it in /utils/index.js
+
+// import the { utils } object using the default import and the { capitalize } and { uppercase } methods using named import on the same line in /students.js
+
+// create a factory function { Person } that takes a name, a last name and an age and returns a new person when called in /students.js
+
+// create another factory function called { Student } that inherits from { Person } with additional argument { University } and returns a new student when applied in /students.js
+
+// convert the { Person } and { Student } factory functions to use the class syntax in /students.js
+
+// add a method to the { Student } class named { celebrate } that returns the string `I've graduated from ${this.university} Yaay!!!` in /students.js
+
+// Create 3 new Students from the class { Student } with different { university } fields in /students.js
+
+// assign to 3 variables the { celebrate } method calls for the 3 Students in /students.js
+
+// log to console the 3 celebratory variables after applying them to the { uppercase } method of the { utils } object
+
+// save the results of all the new files and publish them to your Github
+
+// send me the links until next Monday
+
+// you are welcome to help each other with this project, and remember, google is engineers best friend!
+
+// feel free to hit me with questions whenever you can't find those answers elsewhere
+
+// let me know if there a re typos in this text]]
